ae9baa213857c84547352bbc50f2cb3267f40e28
horizonrz <hossienrezaei1380@gmail.com>
2016-04-05 05:30:48 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {219587559},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
